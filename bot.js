beaea09e801e64a2a23bcae6f5f6c877ed408298
60VoltTransistor <wkforit@gmail.com>
2022-07-31 13:19:30 -0400

    Fixed spelling error

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({
     intents: [
-        /* for those that weren't aware, this is the way you are supposed to use these, I've seen online people have just pu9t these in quotes without values
+        /* for those that weren't aware, this is the way you are supposed to use these, I've seen online people have just put these in quotes without values
            and claim that it has worked for them. However when I tested that, I kept getting errors regarding bits and what not, the correct way is to set this to a value,
            in this case it is a value bit shifted to the left or right x amount of times, you don't have to do it this way, you can just set it equal to a standard integar value
            as well. */
