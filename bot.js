999cc70230d91282fd9cd8669711e3e8001e84a8
60VoltTransistor <wkforit@gmail.com>
2022-07-30 22:46:34 -0400

    Got the bot to show up as online, the tutorial that I was following on how to make a discord bot was outdated. In the end, I found out that the intents part are assigning the values of different variables, of which are later used by the discord.js api to do whatever it does :)

---
+++ bot.js
@@ -0,0 +1,16 @@
+// Setup our environment variables via dotenv
+require('dotenv').config()
+// Import relevant classes from discord.js
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.GUILDS =(1 << 0),
+        GatewayIntentBits.GUILD_MESSAGES=(1 << 9)
+    ]
+});
+// Notify progress
+client.on('ready', function(e){
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+// Authenticate
+client.login(process.env.TOKEN)
