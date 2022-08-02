4f12cca1f49a1e4942de2e6c78e5bc32ccb35cbd
60VoltTransistor <wkforit@gmail.com>
2022-08-01 20:49:27 -0400

    added more code, however I'm still ironing out some bugs/issues, the bot also isn't responding to commands that I give it, still working on getting the commands to work

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // Setup our environment variables via dotenv
 require('dotenv').config();
 // Import relevant classes from discord.js
-const { Client, GatewayIntentBits, IntentsBitField } = require('discord.js');
+const { Client, GatewayIntentBits, IntentsBitField, Collection } = require('discord.js');
 
 const myIntents = new IntentsBitField();
 //add the different intents that you intend to use in this string
@@ -22,9 +22,32 @@ client.on('ready', function(e){
     */
 
 })
-// Authenticate
-client.login(process.env.TOKEN);
 
+//loads out config file
+const fs = require('fs');
+const config = require("./config.json");
+
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+client.commands = new Collection();
+
+const events = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of events) {
+  const eventName = file.split(".")[0];
+  const event = require(`./events/${file}`);
+  client.on(eventName, event.bind(null, client));
+}
+
+const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commands) {
+  const commandName = file.split(".")[0];
+  const command = require(`./commands/${file}`);
+
+  console.log(`Attempting to load command ${commandName}`);
+  client.commands.set(commandName, command);
+}
+
+/* 
 //start of command handler
 client.on("messageCreate", message => {
     if (message.author.bot) return;
@@ -42,3 +65,7 @@ client.on("messageCreate", message => {
       message.channel.send('blah');
     }
   });
+ */
+
+// Authenticate
+client.login(process.env.TOKEN);
---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+const config = require("../config.js");
+
+
+exports.run = (client, message, args) => {
+    message.channel.send("pong!").catch(console.error);
+}
+
+exports.name = "ping";
