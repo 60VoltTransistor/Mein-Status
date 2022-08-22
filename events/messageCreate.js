2c3def086155bd4581e777e5a3ee3a4f38c9e798
60VoltTransistor <wkforit@gmail.com>
2022-08-22 16:46:32 -0400

    Fixed issue where !ping command was causing a reference to 'get' error message, this has now been fixed, make sure to close the issue regarding this problem. All that needed to be done was include the container reference in the code as well as use the correct syntax for getting the command name

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -27,15 +27,28 @@ const logger = require("../modules/logger.js");
 const config = require('../config.json');
 
  module.exports = async(client, message) => {
+
+  const { container } = client;
+
+  if(message.author.bot ){
+    if(config.debug_mode == 'true'){
+      logger.log(`Latest message was sent by a bot`, "warn");
+      return;
+    }
+    else{
+      return;
+    }
+  }
+
   if(message.content.startsWith(config.prefix)){
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const commandName = args.shift();
-    const command = client.commands.get(commandName);
+    const command = container.commands.get(commandName);
     if(!command) return message.channel.send({content:  "That command doesn't exist!"});
     try{
       await command.run(client, message, args);
       if(config.debug_mode == 'true'){
-        console.log("Message >",message.content);
+        logger.log(`Message > ${message.content}`, "debug");
       }
     }
     catch (e) {
